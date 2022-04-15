6623e5c1ab6c429f825be5088ce9fce274b8acf1
Austin <1572296+mrkirby153@users.noreply.github.com>
2022-04-15 15:08:34 -0700

    Initial commit

---
+++ src/main/kotlin/com/mrkirby153/foodandfriends/FoodandfriendsApplication.kt
@@ -0,0 +1,11 @@
+package com.mrkirby153.foodandfriends
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FoodandfriendsApplication
+
+fun main(args: Array<String>) {
+	runApplication<FoodandfriendsApplication>(*args)
+}
---
+++ src/test/kotlin/com/mrkirby153/foodandfriends/FoodandfriendsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mrkirby153.foodandfriends
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FoodandfriendsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
